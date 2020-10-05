7d72b32c04fddf510a10519eef8f7e7ee5140f20
Joabe Silva <joabesilva533@gmail.com>
2020-10-05 16:39:39 -0300

    corrigindo url da api back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://web-app-deploy.herokuapp.com/api'
+    baseURL: 'https://web-app-deploy.herokuapp.com/api'
 });
 
 export default api;
